1062205cf5972b25810f2ecdba08b051b183baf8
amodmurkute <amodmurkute@gmail.com>
2019-01-12 15:08:03 +0530

    navigation added

---
+++ src/app/policy-claim-ui/common/personal-details.model.ts
@@ -0,0 +1,8 @@
+import { Address } from "./address.model";
+
+export class  PersonalDetails {
+    name:string;
+    dob:Date;
+    address:Address;
+    
+}
--- src/app/policy-claim-ui/home/policy/policy.component.ts
+++ src/app/policy-claim-ui/home/policy/policy.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PolicyComponent implements OnInit {
 
+
+
+  navigation=PolicyNavigation;
+  presentPolicy:PolicyNavigation;
   constructor() { }
 
   ngOnInit() {
+    this.presentPolicy=PolicyNavigation.PERSONAL_DETAILS;
+
+  }
+
+  navigationChange(){
+    this.presentPolicy=PolicyNavigation.INCOME_DETAILS;
   }
 
 }
+
+enum PolicyNavigation{
+  PERSONAL_DETAILS,
+  INCOME_DETAILS,
+  NOMINEE_DETAILS,
+  POLICY_DETAILS
+}
+
+
