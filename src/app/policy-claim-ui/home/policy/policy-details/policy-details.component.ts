3cfc58dd3354faeb61959c87d3be0c0c43dd8d1c
amodmurkute <amodmurkute@gmail.com>
2019-01-15 00:26:20 +0530

    autocalculation done

--- src/app/policy-claim-ui/home/policy/policy-details/policy-details.component.ts
+++ src/app/policy-claim-ui/home/policy/policy-details/policy-details.component.ts
@@ -1,6 +1,6 @@
 import { PolicyFormBuilderService } from './../policy-form-builder.service';
 import { Component, OnInit } from '@angular/core';
-import{FormGroup,FormControl} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-policy-details',
@@ -8,12 +8,19 @@ import{FormGroup,FormControl} from '@angular/forms';
   styleUrls: ['./policy-details.component.css']
 })
 export class PolicyDetailsComponent implements OnInit {
-policyFormGroup:FormGroup
-  constructor(private policyFormBuilderService:PolicyFormBuilderService) { }
+  policyDetailsFormGroup: FormGroup
+  constructor(private policyFormBuilderService: PolicyFormBuilderService) { }
 
   ngOnInit() {
     console.log(this.policyFormBuilderService.getPolicyFormGroup())
-    this.policyFormGroup=this.policyFormBuilderService.getPolicyFormGroup().get('policyDetails') as FormGroup;
+    this.policyDetailsFormGroup = this.policyFormBuilderService.getPolicyFormGroup().get('policyDetails') as FormGroup;
+    this.policyDetailsFormGroup.get('premiumFrequency').valueChanges.subscribe(value => {
+      let insurredcAmount = this.policyDetailsFormGroup.get('insurredAmount')
+        && this.policyDetailsFormGroup.get('insurredAmount').value > 0 ? this.policyDetailsFormGroup.get('insurredAmount').value : 1
+      if (insurredcAmount && value) {
+        this.policyDetailsFormGroup.get('maturityAmount').setValue(insurredcAmount*value);
+      }
+    })
   }
 
 }
--- src/app/policy-claim-ui/home/policy/policy-form-builder.service.ts
+++ src/app/policy-claim-ui/home/policy/policy-form-builder.service.ts
@@ -70,4 +70,8 @@ export class PolicyFormBuilderService {
   }
 
 
+
+
+
+
 }
